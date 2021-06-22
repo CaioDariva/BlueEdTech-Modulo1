cf0555f4b73e914cf2d47d2bae4255564385b347
Caio <cgmalavazidariva@gmail.com>
2021-06-22 15:37:49 -0300

    exercicio estoque

--- Aulas/Aula11/Exercicios.py
+++ Aulas/Aula11/Exercicios.py
@@ -23,20 +23,66 @@
 
 # O programa deverá mostrar para o cliente a quantidade de itens comprados e o total.
 
-estoque = {'Papel' : 20, 'Coca' : 15, 'Batata' : 10, 'Cenoura' : 50, 'Chocolate' : 5}
-produto = input('O que você quer comprar? ')
-quantidade = int(input('Quantidade: '))
+estoque = {'TOMATE' : 10, 'ARROZ' : 10, 'BATATA' : 10, 'CENOURA' : 10, 'CHOCOLATE' : 10, 'LEITE' : 10}
 
-retirada = (estoque.pop((produto - quantidade), 'Não encontrado na lista'))
+continuação = 'SIM'
+
+compra = dict()
+compra2 = list()
+
+print('Bem-vindo ao Mercado!')
+print()
+
+while continuação == 'SIM':
+    produto = str(input('O que você gostaria de comprar? ')).upper()
+    print()
+    while produto not in estoque:
+        print('Produto inválido.')
+        produto = input('O que você gostaria de comprar? ').upper()
+        print()
+    retirada = int(input('Qual a quantidade? '))
+    quantidade = estoque.pop(produto)
 
 
+    while retirada > quantidade and quantidade != 0:
+        print(f'Quantidade indisponível. Possuímos apenas {quantidade} unidades.')
+        retirada = int(input('Quantas você deseja? '))
 
 
-# produtos = input('O que você gostaria de comprar: ')
-# quantidade = int(input('Qual quantidade: '))
-# compra = estoque.get(produtos, 'Produto Inválido')
-# compra_total = estoque.pop(quantidade, 'Quantidade indisponível')
-# print(compra)
-# print(compra_total)
-# print(estoque)
-# retirada = estoque(produtos)
+    if retirada <= quantidade:
+        compra[produto] = retirada
+        compra2.append(retirada)
+        estoque[produto] = (quantidade - retirada)
+
+
+    while quantidade == 0:
+        produto = input(f'Esse produto acabou.\nNós ainda temos {estoque}.\nPor favor escolha outro: ').upper()
+        while produto not in estoque:
+            print('Produto inválido.')
+            print()
+            produto = input('O que você gostaria de comprar? ').upper()
+            print()
+        retirada = int(input('Qual a quantidade? '))
+        quantidade = estoque.pop(produto)
+        while retirada > quantidade:
+            print(f'Quantidade indisponível. Possuímos apenas {quantidade} unidades.')
+            retirada = int(input('Quantas você deseja? '))
+        if retirada <= quantidade:
+            compra[produto] = retirada
+            compra2.append(retirada)
+            estoque[produto] = (quantidade - retirada)
+
+
+    print()
+
+    continuação = str(input('Você deseja comprar mais itens? ')).upper().replace('Ã' , 'A')
+    while continuação != 'SIM' and continuação != 'NAO':
+        continuação = str(input('Você deseja comprar mais itens, sim ou não? ')).upper().replace('Ã' , 'A')
+
+print()
+
+
+if continuação == 'NAO':
+    print('Obrigado pelas compras.')
+    print()
+    print(f'Você adquiriru {len(compra)} tipos de itens diferentes e um total de {sum(compra2)} unidades')
